f12cc0fbdbed37c63fc7a40d6e4a0d84f3a72e7a
Vikash <vikashmaurya10@gmail.com>
2022-01-16 17:30:49 -0500

    commit

--- switch case program/P_2.c
+++ switch case program/P_2.c
@@ -1,19 +1,15 @@
+// Calculator program by using swtich case.
 #include <stdio.h>
-
 int main()
 {
     char opt;
-    // int a, b;
-    float a,b,result;
-    printf("Select the oprator( + , - , * , / )  ");
+    float a, b, result;
+    printf("Select the oprator( + , - , * , / ):  ");
     scanf("%c", &opt);
-
     printf("Enter the value of a: ");
     scanf("%f", &a);
-
     printf("Enter the value of b: ");
     scanf("%f", &b);
-    
     switch (opt)
     {
     case '+':
@@ -24,26 +20,21 @@ int main()
         result = a - b;
         printf("substraction a and b is:%.3f\n ", result);
         break;
-
     case '*':
         result = a * b;
         printf("Multiplication a and b is:%.3f\n ", result);
         break;
     case '/':
-
         if (b == 0)
         {
             printf("Input another value of b: ");
-            scanf("%d", &b);
+            scanf("%f", &b);
         }
-        result = a / b;
+        result = a * (1 / b);
         printf("Division a and b is:%.3f\n ", result);
-
         break;
-
     default:
-        printf("something went wrong");
-
+        printf("something went wrong\n");
         return 0;
     }
-}   
+}
