53d9c533e4796a8b7743ffb2d01a25f5416cd700
Vikash <vikashmaurya10@gmail.com>
2022-01-20 22:10:02 -0500

    commit

---
+++ Variable/p_10.c
@@ -0,0 +1,23 @@
+//Examle of static storage class.
+#include <stdio.h>
+void print(void);
+
+int main()
+{
+    printf("First call of print()\n");
+    print();
+    printf("Second call of print()\n");
+    print();
+    printf("Third call of print()\n");
+    print();
+    return 0;
+}
+void print()
+{
+    static int x = 1;
+    int y = 0;
+    printf("Static integer varibale x=%d\n", x);
+    printf("Intger variable y=%d\n", y);
+    x++;
+    y++;
+}
