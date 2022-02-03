12f23d401f6849b84ad5ffef0620ef656aec4f23
Vikash <vikashmaurya10@gmail.com>
2022-02-02 23:49:51 -0500

    commit

---
+++ Pointer/po_1.c
@@ -0,0 +1,18 @@
+//
+#include <stdio.h>
+void main()
+{
+    int x = 10, y = 20;
+    int *ptr, *p;
+    ptr = &x;
+    p = &y;
+    printf("\nValue of x= %d\n", x);
+    printf("Value of *ptr = %d\n", *ptr);
+    printf("Address of x= %x\n", &x);
+    printf("Address of x= %x\n", ptr);
+    printf("-----------------------------\n");
+    printf("Value of y= %d\n", y);
+    printf("Value of *p = %d\n", *p);
+    printf("Address of y= %x\n", &y);
+    printf("Address of y= %x\n", p);
+}
